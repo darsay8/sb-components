527e2ed35816d59b05338d2de6682f4c0f3587f9
darsay8 <darsay.one@gmail.com>
2022-04-25 21:27:33 -0400

    ts config

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Header } from './Header';
@@ -12,7 +11,7 @@ export default {
   },
 } as ComponentMeta<typeof Header>;
 
-const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
+const Template: ComponentStory<typeof Header> = args => <Header {...args} />;
 
 export const LoggedIn = Template.bind({});
 LoggedIn.args = {
--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -17,13 +17,13 @@ export const Basic = Template.bind({});
 Basic.args = { size: 'default', allCaps: false };
 
 export const AllCaps = Template.bind({});
-AllCaps.args = { size: 'normal', allCaps: true };
+AllCaps.args = { size: 'default', allCaps: true };
 
 export const Secondary = Template.bind({});
-Secondary.args = { size: 'normal', color: 'secondary' };
+Secondary.args = { size: 'default', color: 'secondary' };
 
 export const Tertiary = Template.bind({});
-Tertiary.args = { size: 'normal', color: 'tertiary' };
+Tertiary.args = { size: 'default', color: 'tertiary' };
 
 export const CustomFontColor = Template.bind({});
 CustomFontColor.args = { size: 'h1', fontColor: '#5517ac' };
